1de6a93f190a05f696e2f73087b07d1bac02a304
Dellomas <kadellomas@paterostechnologicalcollege.edu.ph>
2025-04-06 16:09:01 +0800

    Finished Activity 1

---
+++ Activity1.java
@@ -0,0 +1,8 @@
+public class Activity1 {
+    public static void main(String[] args) {
+        System.out.println("List of Students:");
+       
+        System.out.println("Kristian Dellomas - BSIT 2I");
+        }
+    }  
+}
